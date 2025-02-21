9cc15831cf4dcf3af48eb093863a708cd69daa3a
Arkar Myint Myat <arkarmyintmyat71@gmail.com>
2025-02-22 01:22:38 +0700

    create employee-list table

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+  employees?: Employee[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.employees = [{
+      id: 1,
+      firstName: "Arkar",
+      lastName: "Myint Myat",
+      email: "arkar123@gmail.com"
+    }]
   }
 
 }
